688d4c6849e0601beae65462472483b0605ed590
Scott Ratigan <scotteratigan@gmail.com>
2020-01-16 10:56:11 -0800

    Logging is working.

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ const path = require('path')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-
-
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -18,8 +16,7 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true, // necessary to get icpMain import in the window
-    },
-
+    }
   });
   mainWindow.webContents.on('did-finish-load', () => {
     // What should I do on load?
@@ -87,13 +84,13 @@ app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
@@ -115,4 +112,4 @@ function hardWire() {
 // hacky, do not like...
 ipcMain.on('asynchronous-message', (event, command) => {
   if (command.startsWith("#connect")) hardWire();
-})
+});

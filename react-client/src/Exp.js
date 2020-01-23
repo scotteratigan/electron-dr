f89ea65f554cf77237e9db0021d422f534b49b32
Scott Ratigan <scotteratigan@gmail.com>
2020-01-23 09:41:56 -0800

    converted exp to table

--- react-client/src/Exp.js
+++ react-client/src/Exp.js
@@ -2,18 +2,27 @@ import React from 'react'
 
 export default function Exp({ exp }) {
   return (
-    <div className="preserve-space monospace">
+    <div className="monospace">
       <h2>Exp:</h2>
-      {Object.keys(exp).map(skill => <ExpItem key={exp[skill].displayName} skill={exp[skill]} />)}
+      <table style={{ width: "100%" }}>
+        <tbody>
+          {Object.keys(exp).map(skill => <ExpItem key={exp[skill].displayName} skill={exp[skill]} />)}
+        </tbody>
+      </table>
     </div>
   )
 }
 
 function ExpItem({ skill }) {
-  const { displayStr, rate } = skill;
+  const { displayName, rank, rate, rateWord } = skill;
   // displayName, rank, rateWord also available in skill
   if (rate === 0) return null;
   return (
-    <div>{displayStr}</div>
+    // <div>{displayStr}</div>
+    <tr>
+      <td>{displayName}</td>
+      <td style={{ textAlign: "right" }}>{parseFloat(rank).toFixed(2)}</td>
+      <td>{rate.toString().padStart(2, '0')}/34</td>
+    </tr>
   )
 }
